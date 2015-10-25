7499baa6e390c0be9cc846cddd73d1a3dcf05cdf
kwst <kwst.andromeda.ma@gmail.com>
2015-10-25 17:49:51 +0900

    create blogapp

--- express_01/blogapp/app.js
+++ express_01/blogapp/app.js
@@ -10,17 +10,33 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride()); // put, deleteを使えるようにするためのmiddleware
+
+// csrf対策
+app.use(express.cookieParser());
+app.use(express.session({secret : 'awsedrftgy123456'}));
+app.use(express.csrf());
+app.use(function(req, res, next) {
+	// csrfトークン
+	res.locals.csrftoken = req.csrfToken();
+	next();
+});
+
 app.use(express.logger('dev'));
 app.use(app.router);
 
+// error処理
+app.use(function(err, req, res, next) {
+	res.send(err.message);
+});
+
 // routing
 app.get('/', post.index);	// 記事一覧
-// app.get('/posts/new', post.new);	// 新規作成
-// app.post('/posts/create', post.create);	// 作成完了
-// app.get('/posts/:id', post.show);	// 記事詳細
-// app.get('/posts/:id/edit', post.edit);	// 記事の編集
-// app.put('/posts/:id', post.update);	// 記事の編集完了
-// app.delete('/posts/:id', post.destroy);	// 記事の編集完了
+app.get('/posts/:id([0-9]+)', post.show);	// 記事詳細
+app.get('/posts/new', post.new);	// 新規作成
+app.post('/posts/create', post.create);	// 作成完了
+app.get('/posts/:id([0-9]+)/edit', post.edit);	// 記事の編集
+app.put('/posts/:id([0-9]+)', post.update);	// 記事の編集完了
+app.delete('/posts/:id', post.destroy);	// 記事の削除
 
 app.listen(3000);
 console.log('server starting ...');
--- express_01/blogapp/routes/post.js
+++ express_01/blogapp/routes/post.js
@@ -1,3 +1,58 @@
+var posts = [
+	{title : 'title0', body : 'body0'},
+	{title : 'title1', body : 'body1'},
+	{title : 'title2', body : 'body2'},
+	{title : 'title3', body : 'body3'}
+];
+
 exports.index = function(req, res) {
-	res.render('posts/index');
+	res.render('posts/index', {posts : posts});
+};
+
+exports.show = function(req, res) {
+	res.render('posts/show', {post : posts[req.params.id]});
+};
+
+exports.new = function(req, res) {
+	res.render('posts/new');
+};
+
+exports.create = function(req, res) {
+	var post = {
+		title : req.body.title,
+		body : req.body.body
+	};
+	posts.push(post);
+	res.render('posts/create', {post : post});
+};
+
+exports.edit = function(req, res) {
+	res.render('posts/edit', {
+		post : posts[req.params.id], 
+		id : req.params.id
+	});
+};
+
+exports.update = function(req, res, next) {
+	if (req.body.id !== req.params.id) {
+		next(new Error('ID not valid'));
+		return;
+	}
+	var id = req.body.id;
+	var post = {
+		title : req.body.title,
+		body : req.body.body
+	};
+	posts[id] = post;
+	res.redirect('/');
+};
+
+exports.destroy = function(req, res, next) {
+	if (req.body.id !== req.params.id) {
+		next(new Error('ID not valid'));
+		return;
+	}
+	var id = req.body.id;
+	posts.splice(id, 1);
+	res.render('posts/destroy');
 };
