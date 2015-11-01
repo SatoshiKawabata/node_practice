131059545bdb03c5d9be05aedd0d8103a3798ec4
kwst <kwst.andromeda.ma@gmail.com>
2015-11-01 17:56:12 +0900

    update tuning_test

--- tuning_test/Gruntfile.js
+++ tuning_test/Gruntfile.js
@@ -1,16 +1,68 @@
 module.exports = function(grunt) {
 	// config
 	grunt.initConfig({
+		// htmlから参照しているプロパティだけを抽出して一つのcssにまとめてくれる	
 		uncss : {
 			dist : {
-				"src/滋賀県立膳所高等学校_files/960_16.css" : ["src/滋賀県立膳所高等学校.html"]
+				files : {
+					'src/files/all.css' : ['src/index.html']
+				}
+			}
+		},
+		// cssを圧縮してくれる
+		csso: {
+			dist: {
+				files: {
+					'src/files/all.min.css': ['src/files/all.css']
+				}
+			}
+		},
+		// jsファイルを一つにまとめてくれる
+		concat : {
+			files : {
+				// 元ファイル
+				src : 'src/files/*.js',
+				dest : 'src/files/all.js'
+			}
+		},
+		// タブインデント・改行を除き、難読化する。
+		uglify : {
+			dist : {
+				files : {
+					// src : 'src/files/all.js',
+					// dest : 'src/files/all.min.js'
+					'src/files/all.min.js' : [
+						// ソース
+						'src/files/all.js'
+					]
+				}
+			}
+		},
+		// htmlを圧縮
+		htmlmin : {
+			dist : {
+				options : {
+					removeComments : true,
+					collapseWhitespace : true,
+					minifyJS : true, // inlineのソースを圧縮
+					minifyCSS : true // inlineのソースを圧縮
+				},
+				files : {
+					'src/index.min.html' : 'src/index.html'
+				}
 			}
 		}
 	});
 
 	// plugin
 	grunt.loadNpmTasks('grunt-uncss');
+	grunt.loadNpmTasks('grunt-csso');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
 	// task
-	grunt.registerTask('default', "uncss");
+	grunt.registerTask('css_min', ["uncss", "csso"]);
+	grunt.registerTask('js_min', ["concat", 'uglify']);
+	grunt.registerTask('html_min', ['htmlmin']);
 }
