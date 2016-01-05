1c8fef1f65db394d05b92fd16b127671336e6c67
kawabata_satoshi <kawabata_satoshi@cyberagent.co.jp>
2016-01-05 19:23:08 +0900

    add react_es6

---
+++ react_es6_01/index.js
@@ -0,0 +1,22 @@
+'use_strict';
+
+import React from 'react'
+
+class CommentBox extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div className='commentBox'>
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+}
+
+React.render(
+  <CommentBox />,
+  document.getElementById('container')
+);
